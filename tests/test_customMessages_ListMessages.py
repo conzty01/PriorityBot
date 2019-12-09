e5350f5dcbe7db864bd8bf8f2629a29f3e3f0b53
conzty01 <conzty01@luther.edu>
2019-12-09 14:27:00 -0600

    Added framework for unittesting

---
+++ tests/test_customMessages_ListMessages.py
@@ -0,0 +1,11 @@
+import unittest
+
+import customMessages as cm
+
+class ListMessage_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
---
+++ tests/test_customMessages_PriorityMessages.py
@@ -0,0 +1,85 @@
+import unittest
+
+import customMessages as cm
+
+class Message_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # ---------- TESTS ----------
+
+    def test_getMessagePayload(self):
+        pass
+
+    def test_getBlocks(self):
+        pass
+
+
+
+class PriorityDirectMessage_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+class PriorityDirectReply_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+class PriorityDirectTimeout_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+class PriorityChannelMessage_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+class PriorityChannelReply_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+class PriorityChannelNotification_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
